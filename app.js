891091a4753a998eef8237ef61c0bca3fdd22806
Igor Siamykin <sparkisisss@gmail.com>
2023-12-06 20:58:01 +0300

    node.js-server

---
+++ app.js
@@ -0,0 +1,61 @@
+const { SerialPort, ReadlineParser } = require('serialport');
+
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+
+app.get('/', (req, res) => {  
+  app.use(express.static(__dirname + '/dist'));
+  res.sendFile(join(__dirname, 'dist', 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
+
+const port = new SerialPort({
+    path:'COM8',  
+    baudRate: 115200,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false
+  }, function (err) {
+    if (err) {
+        return console.log("Error: ", err.message);
+    }
+  })
+
+  port.write('main screen turn on', function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message)
+    }
+    console.log('message written')
+  })
+
+  const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
+  
+  parser.on('data', function (data) {   
+    io.emit('data', data);   
+  })
+
+  
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import './index.html';
 import './index.scss';
 import "@babel/polyfill";
-import './modules/app';
-window.addEventListener('DOMContentLoaded', () => {  
+import { io } from "socket.io-client";
 
+window.addEventListener('DOMContentLoaded', () => {  
+    const socket = io();
+    const temperature = document.getElementById('temperature'),
+        humidity = document.getElementById('humidity'),
+        counter = document.getElementById('counter');        
+
+    let task = [];
+    
+    socket.on('data', (data) => {
+        for (let i = 0; i < 1; i++){
+            task.push(data);
+        }
+        if (task.length == 3) {
+            temperature.innerHTML = task[0];
+            humidity.innerHTML = task[1];
+            counter.innerHTML = task[2];
+            console.log(task);
+            task = [];
+        }
+    });
 });
 
